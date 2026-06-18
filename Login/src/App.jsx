c99bbfeb4c2a4e2279068f5ab3f089ac138d5c6f
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-06-18 21:44:49 +0530

    improving UI

--- Login/src/App.jsx
+++ Login/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-
         <Route
           path="/dashboard"
           element={
